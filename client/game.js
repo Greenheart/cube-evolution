3b8d37cd14f11e6ecc52d35dada5ddb982854cb0
Samuel Plumppu <greenheartmusic@gmail.com>
2015-09-28 02:20:47 +0300

    Created a pretty neat responsive in-game-UI

--- client/game.js
+++ client/game.js
@@ -1,1 +1,20 @@
 // client-side game logic
+
+/* TODO:
+
+  * Create GAME-UI from scratch
+  * Use UI-sketch for reference
+
+*/
+
+// ------------- GAME-UI EVENTS ----------------------
+
+Template.game.events({
+
+});
+
+// ------------- GAME-UI HELPERS ---------------------
+
+Template.game.helpers({
+
+});
