63a1a4969e19b133143784df3bb72aa02385f4a7
Samuel Plumppu <greenheartmusic@gmail.com>
2015-09-28 12:53:20 +0300

    The UI now handles logout-events properly

--- client/client.js
+++ client/client.js
@@ -18,6 +18,12 @@ Template.main.helpers({
   }
 });
 
+Template.loginButtons.events({
+  'click #login-buttons-logout': function() {
+    Session.set("inGame", false);
+  }
+});
+
 Template.startScreen.events({
   'click input.startGame': function(event) {
     event.preventDefault();
@@ -36,9 +42,3 @@ function userHasCube() {
   // check if the current user has a cube
   return Cubes.findOne({ owner: Meteor.userId() });
 }
-
-
-/* TODO:
-    * Handle user-logout events + user closing window (window.onbeforeunload like in TicTacToe)
-      * Set Session-var "inGame" to false
-*/
